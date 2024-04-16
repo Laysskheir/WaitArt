d4f8a9b1b414eb97a54b09a0112cacf2df9e387a
Layss kheir <laysskheir@gmail.com>
2024-04-16 20:06:32 +0300

    fix api emails

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -1,15 +1,15 @@
-// /api/send.ts
-
 import { Resend } from "resend";
 import { SubmissionsEmail } from "@/components/email-template";
 import * as React from "react";
 import currentUser from "@/actions/current-user";
+import { NextRequest, NextResponse } from "next/server";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 // Function to send email
-export async function sendEmail(toEmail: string) {
+export async function POST(req: NextRequest) {
   try {
+    const { toEmail } = await req.json();
     const user = await currentUser();
     const { data, error } = await resend.emails.send({
       from: "Admin <onboarding@resend.dev>",
@@ -23,9 +23,9 @@ export async function sendEmail(toEmail: string) {
     if (error) {
       console.error("Error sending email:", error);
     }
-
-    console.log("Email sent successfully:", data);
+    return new NextResponse(JSON.stringify(data), { status: 200 });
   } catch (error) {
     console.error("Error sending email:", error);
+    return new NextResponse(JSON.stringify({ error }), { status: 500 });
   }
 }
