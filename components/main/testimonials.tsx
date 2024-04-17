f74e066015c96331ee950d2e90c24678fbb6f962
Layss kheir <laysskheir@gmail.com>
2024-04-17 17:54:04 +0300

    fix testimonials

--- components/main/testimonials.tsx
+++ components/main/testimonials.tsx
@@ -1,15 +1,14 @@
 "use client";
 
-import { InfiniteMovingCards } from "../ui/infinite-moving-cards";
 
 export function Testimonials() {
   return (
     <div className="h-[40rem] rounded-md flex flex-col antialiased bg-white dark:bg-black dark:bg-grid-white/[0.05] items-center justify-center relative overflow-hidden">
-      <InfiniteMovingCards
+      {/* <InfiniteMovingCards
         items={testimonials}
         direction="right"
         speed="slow"
-      />
+      /> */}
     </div>
   );
 }
